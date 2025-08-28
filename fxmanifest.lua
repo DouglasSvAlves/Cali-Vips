f2c00e4da96cf9b3e704eb4f9839b2439878abff
Douglas Silva Alves <149844295+DouglasSvAlves@users.noreply.github.com>
2025-08-28 20:15:04 -0300

    Manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+
+fx_version 'cerulean'
+game 'gta5'
+
+name "California Vips"
+author "Douglas S.Alves"
+version "1.1"
+
+client_scripts {
+	'config.lua',
+	'cl_vips.lua'
+}
+
+server_scripts {
+	'config.lua',
+	'sv_vips.lua'
+}
